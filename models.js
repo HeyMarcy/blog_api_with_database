d41d0c881146543e5dcaa23c47302c733d65ab46
marcy <marcy.montross@gmail.com>
2017-02-23 16:18:47 -0600

    create virtual author property

--- models.js
+++ models.js
@@ -3,19 +3,21 @@ const mongoose = require('mongoose');
 const blogPostSchema = mongoose.Schema({
   title: {type: String, required: true},
   content: String,
-  author: String,
-  // author: {type: String, required: true},
-  publishDate: {type: Number, required: true},
+  author: {firstName: {type: String, required: true},lastName: {type: String, required: true}},
+  publishDate: {type: Number, required: true}
 });
 
 
 // add byline with virtual
+blogPostSchema.virtual('authorString').get(function(){
+  return `${this.author.firstName} ${this.author.lastName}`.trim();
+})
 
 blogPostSchema.methods.apiRepr = function() {
     return {
       title: this.title,
       content: this.content,
-      author: this.author,
+      author: this.authorString,
       publishDate: this.publishDate,
       _id: this._id
     }
